a29f5b58489e82ca7f0b67d579fe8437d4e6d253
Elizaveta Khlebnikova <elizavetakhlebnikova@gmail.com>
2024-05-17 23:27:21 +0300

    настроено отображение картинок в админ-панели

--- space_agency/landing/admin.py
+++ space_agency/landing/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ImageForMainPageModel
+from django.utils.safestring import mark_safe
+from adminsortable2.admin import SortableAdminMixin
 
-# Register your models here.
+@admin.register(ImageForMainPageModel)
+class ImageForMainPageModelAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ('name', 'image_preview')
+
+    @admin.display(description="предварительный просмотр изображения")
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" width="100px" height="100px">'.format(obj.image.url))
+        return 'нет изображения'
+
+    image_preview.allow_tags = True
