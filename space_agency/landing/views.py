a50bbc9bb37efaac5c7cd60691c3c3d7294012d8
Elizaveta Khlebnikova <elizavetakhlebnikova@gmail.com>
2024-05-17 23:51:34 +0300

    создан класс-представление для главной страницы

--- space_agency/landing/views.py
+++ space_agency/landing/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import ImageForMainPageModel
 
-# Create your views here.
+
+class LandingPageView(TemplateView):
+    template_name = 'landing/landing.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LandingPageView, self).get_context_data(**kwargs)
+        context['object_list'] = ImageForMainPageModel.objects.order_by('my_order')
+        context['title'] = 'NASA - космическое агентство'
+        return context
