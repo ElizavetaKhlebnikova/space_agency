09825e0c55f5906d2f59aa18c95798b1b27a1b60
Elizaveta Khlebnikova <elizavetakhlebnikova@gmail.com>
2024-05-20 06:49:53 +0300

    добавлен перевод модели и полей на русский

--- space_agency/landing/models.py
+++ space_agency/landing/models.py
@@ -3,8 +3,8 @@ from filer.fields.image import FilerImageField
 
 
 class ImageForMainPageModel(models.Model):
-    name = models.CharField(max_length=255, default='')
-    image = FilerImageField(related_name='изображение', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255, default='', verbose_name=u"Название картинки")
+    image = FilerImageField(related_name='image', on_delete=models.CASCADE, null=True, blank=True, verbose_name=u"Картинка")
     my_order = models.PositiveIntegerField(
         default=0,
         blank=False,
@@ -13,6 +13,8 @@ class ImageForMainPageModel(models.Model):
 
     class Meta:
         ordering = ['my_order']
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
 
     def __str__(self):
         return f'Image ID: {self.image.id}, order: {self.my_order}'
